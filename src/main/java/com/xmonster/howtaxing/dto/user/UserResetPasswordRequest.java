2e6754561fd34e52061449eff6c61db351c41e8a
joosaebak <joosaebak@naver.com>
2024-11-27 00:27:50 +0900

    1. 비밀번호 재설정 관련 로그 추가 -김웅태

--- src/main/java/com/xmonster/howtaxing/dto/user/UserResetPasswordRequest.java
+++ src/main/java/com/xmonster/howtaxing/dto/user/UserResetPasswordRequest.java
@@ -1,14 +1,12 @@
 package com.xmonster.howtaxing.dto.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
 public class UserResetPasswordRequest {
     private String phoneNumber;         // [필수] 휴대폰번호(ex:01012345678)
     private String authKey;             // [필수] 인증키(SMS인증 후 받은 인증키 30자리)
