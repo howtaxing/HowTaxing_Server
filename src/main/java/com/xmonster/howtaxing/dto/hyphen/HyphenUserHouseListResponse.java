81a1fbc04dc27a3935aa00602b68bef69f964667
joosaebak <joosaebak@naver.com>
2024-12-12 20:24:34 +0900

    1. 하이픈 공통부 수정에 따른 변경사항 적용 -김웅태

--- src/main/java/com/xmonster/howtaxing/dto/hyphen/HyphenUserHouseListResponse.java
+++ src/main/java/com/xmonster/howtaxing/dto/hyphen/HyphenUserHouseListResponse.java
@@ -22,13 +22,15 @@ public class HyphenUserHouseListResponse {
     @AllArgsConstructor
     public static class HyphenCommon {
         @JsonProperty("userTrNo")
-        private String userTrNo;
+        private String userTrNo;            // 사용자 거래고유번호(사용자 생성)
         @JsonProperty("hyphenTrNo")
-        private String hyphenTrNo;
+        private String hyphenTrNo;          // 하이픈 고유거래번호(하이픈 생성)
         @JsonProperty("errYn")
-        private String errYn;
+        private String errYn;               // 오류여부(Y:오류, N:정상)
+        @JsonProperty("errCd")
+        private String errCd;               // 에러코드
         @JsonProperty("errMsg")
-        private String errMsg;
+        private String errMsg;              // 응답메세지
     }
 
     @Data
