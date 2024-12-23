9531b416450d450c6a3f636d7782c400e1c50cf0
HowTaxing <joosaebak@naver.com>
2024-12-24 01:01:21 +0900

    상담자 목록 조회 쿼리 개선
    1. 상담자 목록 조회 쿼리 개선
     : 상담가능한 상태의 상담자만 조회하도록 쿼리 추가
    - 김웅태

--- src/main/java/com/xmonster/howtaxing/repository/consulting/ConsultantInfoRepository.java
+++ src/main/java/com/xmonster/howtaxing/repository/consulting/ConsultantInfoRepository.java
@@ -7,5 +7,8 @@ import java.util.Optional;
 
 public interface ConsultantInfoRepository extends JpaRepository<ConsultantInfo, Long> {
 
+    @Query(value = "SELECT * FROM consultant_info c WHERE c.is_consulting_available = true ORDER BY consultant_id ASC", nativeQuery = true)
+    List<ConsultantInfo> findAvailableConsultantList();
+    
     Optional<ConsultantInfo> findByConsultantId(Long consultantId);
 }
