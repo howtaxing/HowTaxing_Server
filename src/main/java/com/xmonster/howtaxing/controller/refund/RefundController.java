d38c98ef156c774c472a37c063244248bbedcc1e
joosaebak <joosaebak@naver.com>
2025-01-19 16:02:24 +0900

    1. 환급상담신청 RequestBody 어노테이션 추가 -김웅태

--- src/main/java/com/xmonster/howtaxing/controller/refund/RefundController.java
+++ src/main/java/com/xmonster/howtaxing/controller/refund/RefundController.java
@@ -5,6 +5,7 @@ import com.xmonster.howtaxing.service.refund.RefundService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +16,7 @@ public class RefundController {
 
     // (양도소득세)환급상담신청
     @PostMapping("/refund/apply")
-    public Object applyRefundConsulting(RefundConsultingApplyInfoRequest refundConsultingApplyInfoRequest) throws Exception {
+    public Object applyRefundConsulting(@RequestBody RefundConsultingApplyInfoRequest refundConsultingApplyInfoRequest) throws Exception {
         log.info(">> [Controller]RefundController applyRefundConsulting - (양도소득세)환급상담신청");
         return refundService.applyRefundConsulting(refundConsultingApplyInfoRequest);
     }
